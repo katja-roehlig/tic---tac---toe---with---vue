5f9598293eae4a3aba5b6a34b5a803b12823962d
katja-roehlig <katja-roehlig@mail.de>
2022-11-21 20:04:14 +0100

    feat: basic functionality

--- script.js
+++ script.js
@@ -1,6 +1,30 @@
 const { createApp } = Vue;
 Vue.createApp({
     data(){return{
-        
-    }}
+        winner: false,
+        a:"",
+        b: "",
+        c: "",
+        d: "",
+        e: "",
+        f: "",
+        g:"",
+        h:"",
+        i:"",
+
+    }},
+computed:{
+        compareAreas(){
+            if(this.a !== "" && this.a === this.b && this.a === this.c ||
+           this.d !== "" && this.d === this.e && this.d === this.f ||
+           this.g !== "" && this.g === this.h && this.g === this.i ||
+           this.a !== "" && this.a === this.e && this.a === this.i ||
+           this.c !== "" && this.c === this.e && this.c === this.g
+           
+           ){
+           return this.winner = true;
+           };
+    }
+    
+    }
 }).mount("#app");
